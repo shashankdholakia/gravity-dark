52cf018af8c8fca461fa6e483d293c3da37ff784
Shashank Dholakia <shashankdholakia1@gmail.com>
2021-03-20 02:15:36 -0700

    Analytic sT integral and some tests in notebook

--- utils/__init__.py
+++ utils/__init__.py
@@ -6,6 +6,7 @@ Created on Tue Jan 26 21:49:41 2021
 @author: shashank
 """
 
-from .gdark import f, planck, flux_oblate
-from .integration import G, g, sT, semi_analytic_sT, compute_disk, compute_xi, compute_phi, draw_oblate, draw_oblate_full
+from .gdark import f, planck, flux_oblate, f_eff, flux_spherical
+from .integration import G, g, sT, semi_analytic_sT, analytic_sT, compute_disk, compute_xi, compute_phi, draw_oblate, draw_oblate_full, tT_numerical
+from .integration import Solver
 from .plot_spheroid import plot_one_spheroid_inc
